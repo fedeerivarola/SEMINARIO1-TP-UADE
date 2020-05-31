222f45faa938d3a89ba99b6b1ff0ebb9e4cb30e1
Braian Filipovic <bfilipovic@grupoassa.com>
2020-05-31 17:26:06 -0300

    Componente SideDrawer v2 (Falta integracion react-router)

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,15 +1,38 @@
 import React from 'react'
 import './SideDrawer.css'
+import Link from 'react-router-dom'
+
+//Imagenes
+import resumenLogo from '../../assets/sd-resumen.png';
+import comercioLogo from '../../assets/sd-comercio.png';
+import niñosLogo from '../../assets/sd-niños.png';
+import saldoLogo from '../../assets/sd-saldo.png';
+import movimientosLogo from '../../assets/sd-movimientos.png';
 
 const sideDrawer = props => {
     return(
         <div className="SideDrawer">
             <div className="List">
-                <a active href="/home">Resumen</a>
-                <a href="/home"><li>Saldo</li></a>
-                <a href="/home"><li>Movimientos</li></a>
-                <a href="/home"><li>Niños</li></a>
-                <a href="/home"><li>Locales</li></a>
+                <a active href="/home">
+                    <img src={resumenLogo} alt=""></img>
+                    Resumen
+                </a>
+                <a href="/home">
+                    <img src={saldoLogo} alt=""></img>
+                    Saldo
+                </a>
+                <a href="/home">
+                    <img src={movimientosLogo} alt=""></img>
+                    Movimientos
+                </a>
+                <a href="/home">
+                    <img src={niñosLogo} alt=""></img>
+                    Niños
+                </a>
+                <a href="/home">
+                    <img src={comercioLogo} alt=""></img>
+                    Comercios
+                </a>
             </div>
         </div>
     );
