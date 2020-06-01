be0d5eaaf3e1122dd1383f8811cd25b061894e95
Federico Rivarola <frivarola@lat.ar>
2020-06-01 19:34:27 -0300

    merge

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ import App from './App';
 
 
 ReactDOM.render(
-<<<<<<< HEAD
       <App />,
-=======
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
->>>>>>> master
   document.getElementById('root')
 );
