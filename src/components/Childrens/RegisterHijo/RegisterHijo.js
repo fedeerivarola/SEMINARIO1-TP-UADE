c271a57990a727cd57979d0eafcba6e639cd76d4
Federico A. Rivarola <federicorivarola@outlook.com>
2020-06-15 20:21:04 -0300

    se creo pantalla de registrar hijo

---
+++ src/components/Childrens/RegisterHijo/RegisterHijo.js
@@ -0,0 +1,165 @@
+import React from 'react';
+import './RegisterHijo.css'
+import { RadioGroup, Radio, FormControlLabel } from '@material-ui/core';
+import { dbPadres, dbHijos, avatar } from '../../../services/firebase/config'
+
+
+class RegisterHijo extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            padre: props.padreRH,
+            errorMessage: null
+        }
+        this.fileInput = React.createRef();
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (this.state.user) {
+            if (this.state.nombre) {
+                if (this.state.edad) {
+                    if(localStorage.getItem('avatarURL') && (this.fileInput.current.files[0])){
+
+                    
+                    let celiac, diabetic;
+
+                    this.state.esCeliaco === "si" ? celiac = true : celiac = false;
+                    this.state.esDiabetico === "si" ? diabetic = true : diabetic = false;
+
+
+                    let newHijo = {
+                        nombre: this.state.nombre,
+                        user: this.state.user,
+                        edad: this.state.edad,
+                        esCeliaco: celiac,
+                        esDiabetico: diabetic,
+                        padre: this.state.padre.mail,
+                        saldoAsignado: 0,
+                        avatar: localStorage.getItem('avatarURL')
+                    }
+
+                    console.log(newHijo);
+                    let refHijos = dbPadres.doc(this.state.padre.mail).collection("hijos");
+
+                    refHijos.doc(newHijo.user).set(newHijo)
+                        .then(() => {
+                            dbHijos.doc(newHijo.user).set(newHijo)
+                                .then(() => {
+                                    console.log("se registro el hijo, hay qe meter un push a la pagina aca");
+                                })
+                                .catch((error) => { this.setState({ errorMessage: error }) })
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                            this.setState({ errorMessage: error });
+                        })
+                    } else {this.setState({ errorMessage: "Aun estamos cargando la imagen :( vuelva a intentar" });}
+                } else { this.setState({ errorMessage: "Debe ingresar una edad" }); }
+            } else { this.setState({ errorMessage: "Debe ingresar un nombre" }); }
+        } else { this.setState({ errorMessage: "Debe ingresar un usuario" }); }
+
+    }
+
+    fileInputHandler = (e) => {
+
+        if (this.fileInput.current.files[0]) {
+            if (this.state.user) {
+                let newAvatar = avatar.child(`${this.state.user}.jpg`);
+                var uploadTask = newAvatar.put(this.fileInput.current.files[0]);
+                var avatarURL = null;
+
+                uploadTask.on('state_changed', function (snapshot) {
+                    // Observe state change events such as progress, pause, and resume
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                }, function (error) {
+                    this.setState({ errorMessage: error.errorMessage });
+                }, function () {
+                    // Handle successful uploads on complete
+                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                    uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+                        console.log(downloadURL);
+                        localStorage.setItem('avatarURL', downloadURL);
+                    });
+                });
+
+            } else {
+                this.setState({ errorMessage: "Primero complete el usuario por favor" })
+            }
+        }
+
+    }
+
+    nameInputHandler = (e) => {
+        this.setState({
+            nombre: e.target.value
+        })
+    }
+
+    userInputHandler = (e) => {
+        this.setState({
+            user: e.target.value
+        })
+    }
+
+    ageInputHandler = (e) => {
+        this.setState({
+            edad: e.target.value
+        })
+    }
+
+    celiacRadioHandler = (e) => {
+        this.setState({
+            esCeliaco: e.target.value
+        })
+    }
+
+    diabeticRadioHandler = (e) => {
+        this.setState({
+            esDiabetico: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className="RegisterHijo">
+                <h2>Complete datos del hijo a registrar</h2>
+                <form className="formRegisterHijo" onSubmit={this.handleSubmit}>
+                    <p>Nombre</p>
+                    <input required onChange={(event) => this.nameInputHandler(event)} />
+                    <p>Usuario</p>
+                    <input required onChange={(event) => this.userInputHandler(event)} />
+                    <p>Edad</p>
+                    <input required type="number" onChange={(event) => this.ageInputHandler(event)} />
+                    <p>Es celiaco?</p>
+                    <RadioGroup onChange={(event) => this.celiacRadioHandler(event)}>
+                        <FormControlLabel value="si" control={<Radio />} label="Si" />
+                        <FormControlLabel value="no" control={<Radio />} label="No" />
+                    </RadioGroup>
+                    <p>Es diabetico?</p>
+                    <RadioGroup onChange={(event) => this.diabeticRadioHandler(event)}>
+                        <FormControlLabel value="si" control={<Radio />} label="Si" />
+                        <FormControlLabel value="no" control={<Radio />} label="No" />
+                    </RadioGroup>
+                    <p>Avatar</p>
+                    <input type="file" ref={this.fileInput} onChange={(event) => this.fileInputHandler(event)} />
+                    {/* <button type="submit">REGISTRAR</button> */}
+                </form>
+                {
+                    this.state.errorMessage &&
+                    <div className="alert alert-danger" role="alert">
+                        <span style={{ color: "red" }}>Error:{this.state.errorMessage}</span>
+                    </div>
+                }
+                <button onClick={this.handleSubmit}>REGISTRAR</button>
+            </div>
+        );
+    }
+}
+
+
+export default RegisterHijo;
